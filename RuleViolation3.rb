e71309528e045304d42c7a5414b908cff6d4ccd3
Rodolfo Silva <rasilvap22@gmail.com>
2022-11-23 13:26:10 -0500

    Update RuleViolation3.rb

--- RuleViolation3.rb
+++ RuleViolation3.rb
@@ -12,7 +12,7 @@ def bad_deserialization
     data = YAML.dump(o)
     # ok: bad-deserialization-yaml
     obj = YAML.load(data, safe: true)
-    obj23 = YAML.load(data, safe: true)
+    obj24 = YAML.load(data, safe: true)
     
 
     filename = File.read("test.txt")
